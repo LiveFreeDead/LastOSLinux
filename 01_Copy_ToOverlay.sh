0a05c45653775a541a27913b922a93f68a4c24a2
LiveFreeDead <LiveFreeDead@hotmail.com>
2024-09-16 10:19:28 +1000

    Kept a few extra overlay settings prior to copying from running system to overlay

--- 01_Copy_ToOverlay.sh
+++ 01_Copy_ToOverlay.sh
@@ -3,6 +3,15 @@
 mkdir -p ./OS_Overlay/etc/skel/.local/share/nemo/scripts
 cp -rf $HOME/.local/share/nemo/scripts/* ./OS_Overlay/etc/skel/.local/share/nemo/scripts
 
+#Copy grouped window to main user first, so it keeps chrome and Cycle windows
+mkdir -p $HOME/.config/cinnamon/spices/grouped-window-list@cinnamon.org/
+cp -f ./OS_Overlay/etc/skel/.config/cinnamon/spices/grouped-window-list@cinnamon.org/2.json $HOME/.config/cinnamon/spices/grouped-window-list@cinnamon.org/
+
+mkdir -p $HOME/.config/cinnamon/spices/calendar@cinnamon.org/
+cp -f ./OS_Overlay/etc/skel/.config/cinnamon/spices/calendar@cinnamon.org/13.json $HOME/.config/cinnamon/spices/calendar@cinnamon.org/
+
+
+
 mkdir -p ./OS_Overlay/etc/skel/.config/cinnamon/spices
 cp -rf $HOME/.config/cinnamon/spices/* ./OS_Overlay/etc/skel/.config/cinnamon/spices
 
