e2613ca90ddfa7e6cbe27b54f2c4e79036d1dc0c
LiveFreeDead <LiveFreeDead@hotmail.com>
2024-09-26 13:42:43 +1000

    Added LLFile Templates to all users

---
+++ OS_Overlay/etc/skel/Templates/LLScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Add "org.name.thing" below and unremark to install a Flatpak
+#x-terminal-emulator -e "flatpak install --system -y --noninteractive flathub "
+
+
---
+++ OS_Overlay/etc/skel/Templates/LLScript_Sudo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Add package names and unremark to install from apt
+#sudo apt install -y 
+
+
