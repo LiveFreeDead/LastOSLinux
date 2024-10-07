5043c871dff5e0c765e6af6eea5fbd9b944e4d9e
LiveFreeDead <LiveFreeDead@hotmail.com>
2024-10-07 14:57:18 +1100

    Added multiple ways to install FlatHub

---
+++ OS_Overlay/LastOS/Scripts/AddFlathubRepo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Adding Flathub Repository"
+#Enable flathub repo's
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+sleep 1
